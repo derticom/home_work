51bb75ecbff192a2fd6d163c502705d1ec160754
egorovm <m.egorov@t1-group.ru>
2024-09-28 23:58:01 +0300

    Implement copy

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-	"errors"
+	"io"
+	"os"
+
+	"github.com/pkg/errors"
 )
 
 var (
@@ -10,6 +13,58 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	sourceFile, err := os.OpenFile(fromPath, os.O_RDONLY, 0644)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+	defer sourceFile.Close()
+
+	sourceFileInfo, err := sourceFile.Stat()
+	if err != nil {
+		return errors.Wrap(err, "failed to sourceFile.Stat")
+	}
+
+	// offset больше, чем размер файла - невалидная ситуация
+	if sourceFileInfo.Size() < offset {
+		return ErrOffsetExceedsFileSize
+	}
+
+	// программа может НЕ обрабатывать файлы, у которых неизвестна длина (например, /dev/urandom)
+	if sourceFileInfo.Size() == 0 {
+		return errors.New("unknown len source file")
+	}
+
+	if limit > sourceFileInfo.Size()-offset {
+		limit = sourceFileInfo.Size() - offset
+	}
+
+	if limit == 0 {
+		limit = sourceFileInfo.Size() - offset
+	}
+
+	_, err = sourceFile.Seek(offset, 0)
+	if err != nil {
+		return errors.Wrap(err, "failed to sourceFile.Seek")
+	}
+
+	buf := make([]byte, limit)
+
+	var totalRead int64
+	for totalRead != limit {
+		n, err := sourceFile.Read(buf[totalRead:])
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return errors.Wrap(err, "failed to sourceFile.Read")
+		}
+		totalRead += int64(n)
+	}
+
+	err = os.WriteFile(toPath, buf, 0755)
+	if err != nil {
+		return errors.Wrap(err, "failed to os.WriteFile")
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,118 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	fromPath = "./testdata/input.txt"
+	toPath   = "./tmp/input.txt"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	type args struct {
+		fromPath string
+		toPath   string
+		offset   int64
+		limit    int64
+	}
+	tests := []struct {
+		name     string
+		args     args
+		wantErr  bool
+		wantFile string
+	}{
+		{
+			name: "offset 0, limit 0",
+			args: args{
+				fromPath: fromPath,
+				toPath:   toPath,
+				offset:   0,
+				limit:    0,
+			},
+			wantErr:  false,
+			wantFile: "testdata/out_offset0_limit0.txt",
+		},
+		{
+			name: "offset 0, limit 10",
+			args: args{
+				fromPath: fromPath,
+				toPath:   toPath,
+				offset:   0,
+				limit:    10,
+			},
+			wantErr:  false,
+			wantFile: "testdata/out_offset0_limit10.txt",
+		},
+		{
+			name: "offset 0, limit 1000",
+			args: args{
+				fromPath: fromPath,
+				toPath:   toPath,
+				offset:   0,
+				limit:    1000,
+			},
+			wantErr:  false,
+			wantFile: "testdata/out_offset0_limit1000.txt",
+		},
+		{
+			name: "offset 0, limit 10000",
+			args: args{
+				fromPath: fromPath,
+				toPath:   toPath,
+				offset:   0,
+				limit:    10000,
+			},
+			wantErr:  false,
+			wantFile: "testdata/out_offset0_limit10000.txt",
+		},
+		{
+			name: "offset 100, limit 1000",
+			args: args{
+				fromPath: fromPath,
+				toPath:   toPath,
+				offset:   100,
+				limit:    1000,
+			},
+			wantErr:  false,
+			wantFile: "testdata/out_offset100_limit1000.txt",
+		},
+		{
+			name: "offset 6000, limit 1000",
+			args: args{
+				fromPath: fromPath,
+				toPath:   toPath,
+				offset:   6000,
+				limit:    1000,
+			},
+			wantErr:  false,
+			wantFile: "testdata/out_offset6000_limit1000.txt",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := os.Mkdir("./tmp", 0755)
+			require.NoError(t, err)
+
+			defer func() {
+				err = os.RemoveAll("tmp")
+				require.NoError(t, err)
+			}()
+
+			err = Copy(tt.args.fromPath, tt.args.toPath, tt.args.offset, tt.args.limit)
+			assert.NoError(t, err)
+
+			gotFile, err := os.ReadFile(tt.args.toPath)
+			require.NoError(t, err)
+
+			wantFile, err := os.ReadFile(tt.wantFile)
+			require.NoError(t, err)
+
+			assert.Equal(t, wantFile, gotFile)
+		})
+	}
 }
